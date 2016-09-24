88f13463d5c4806d90befa07d6ab123aece1e432
Sir Nightmare <ivantochilkin@gmail.com>
2016-09-24 05:01:42 +0300

    the task is done

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,20 @@ import json
 
 
 def load_data(filepath):
-    pass
+    data = ""
+    with open(filepath, 'r', encoding="utf8") as file:
+        data = file.read()
+    return json.loads(data)
 
 
 def pretty_print_json(data):
-    pass
+    '''
+    God bless python documentation! :D
+    '''
+    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('alco_shops.json')
+    data = load_data(input('Type the path to json file'))
+    pretty_print_json(data)
